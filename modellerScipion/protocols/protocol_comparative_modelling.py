700224f093b1ca85782fb24b9340ab9dcb4f8e24
DaniDelHoyo <danieldelhoyog@gmail.com>
2022-07-15 19:14:53 +0200

    Multiple chain modelling

--- modellerScipion/protocols/protocol_comparative_modelling.py
+++ modellerScipion/protocols/protocol_comparative_modelling.py
@@ -32,7 +32,7 @@ alignable known structures
 
 """
 
-import os, json, shutil
+import os, json, shutil, glob
 from pyworkflow.protocol import params
 from pyworkflow.utils import Message
 from pwem.protocols import EMProtocol
@@ -62,12 +62,24 @@ class ModellerComparativeModelling(EMProtocol):
                       label='Template PDB: ', help='Specify the PDB code to use as template')
 
         form.addParam('tempChain', params.StringParam,
-                      label='Template chain: ',
+                      label='Template chain: ', condition='not multiChain',
                       help='Specify the protein chain to use as template. If None, modeller will try to guess it')
+
         form.addParam('tempPositions', params.StringParam,
-                      label='Template positions: ',
+                      label='Template positions: ', condition='not multiChain',
                       help='Specify the positions of the sequence to use in the alignment. '
                            'If None, modeller will use the whole sequence')
+
+        form.addParam('tempMChain', params.StringParam,
+                      label='Template multi chain: ', condition='multiChain',
+                      help='Specify the protein chains to use as template (Use CTRL from multiple selection). '
+                           'If None, modeller will try to guess it')
+        form.addParam('tempMPositions', params.StringParam, expertLevel=params.LEVEL_ADVANCED,
+                      label='Template multi positions: ', condition='multiChain',
+                      help='Specify the positions of each of the chains to use in the alignment.\n'
+                           'In the same order as in "Template multi chain: ", write the first and last index '
+                           'comma-separated. e.g: 1-30, 4-35, 20-55\n '
+                           'If None, FIRST-LAST will be used')
 
         form.addParam('addTemplate', params.LabelParam,
                       label='Add template',
@@ -76,10 +88,21 @@ class ModellerComparativeModelling(EMProtocol):
     def _defineParams(self, form):
         """ """
         form.addSection(label=Message.LABEL_INPUT)
+        form.addParam('multiChain', params.BooleanParam, default=False,
+                      label="Multiple chains: ", expertLevel=params.LEVEL_ADVANCED,
+                      help='Build a comparative modelling using several chains. \nThis way, the input must be the '
+                           'target sequences of each of the chains selected from the templates '
+                           '(and in the same order!).\nYou can build this set of sequences using the '
+                           '"define set of sequences" protocol')
         form.addParam('inputSequence', params.PointerParam,
-                       pointerClass='Sequence', allowsNull=False,
-                       label="Input sequence to predict: ",
+                       pointerClass='Sequence', allowsNull=True,
+                       label="Input sequence to predict: ", condition='not multiChain',
                        help='Select the sequence whose atomic structure will be predicted')
+        form.addParam('inputSequences', params.PointerParam,
+                      pointerClass='SetOfSequences', allowsNull=True,
+                      label="Input sequences to predict: ", condition='multiChain',
+                      help='Select the sequences whose atomic structure will be predicted')
+
         form.addParam('adIni', params.BooleanParam, default=False,
                       label="Use initial model: ",
                       help='Use structure as initial model. It must have the same sequence as the target')
@@ -108,10 +131,16 @@ class ModellerComparativeModelling(EMProtocol):
                             "2,3,4) Scipion-chem included programs with default params\n"
                             "5) Custom alignment as SetOfSequences aligned")
         
-        group.addParam('inputAlign', params.PointerParam, pointerClass='SetOfSequencesChem',
-                       label='Input sequence alignment: ', condition='alignMethod == 4', allowsNull=True,
+        group.addParam('inputAlign', params.PointerParam, pointerClass='SetOfSequencesChem', allowsNull=True,
+                       label='Input sequence alignment: ', condition='alignMethod==4 and not multiChain',
                        help="Input sequence alignment containing all the sequences specified "
                             "in this protocol formulary (both templates and target).\n")
+
+        group.addParam('inputAlignFile', params.PathParam,
+                       label='Input alignment file: ', condition='alignMethod==4 and multiChain', allowsNull=True,
+                       help="Input sequence alignment containing all the sequences and chains in PIR format as"
+                            " expected from modeller. https://salilab.org/modeller/manual/node29.html")
+
 
         form.addSection(label='Other parameters')
         form.addParam('modelH', params.BooleanParam, default=False,
@@ -137,13 +166,15 @@ class ModellerComparativeModelling(EMProtocol):
     # --------------------------- STEPS functions ------------------------------
     def _insertAllSteps(self):
         # Insert processing steps
+        self._insertFunctionStep('alignStep')
         self._insertFunctionStep('modellerStep')
         self._insertFunctionStep('createOutputStep')
 
-    def modellerStep(self):
-        pdbsFile = self.buildPDBsFile()
+    def alignStep(self):
         alignFile = self.buildAlignFile()
 
+    def modellerStep(self):
+        pdbsFile = self.buildPDBsFile()
         Plugin.runModeller(self, Plugin.getScriptsDir('comparative_modelling.py'),
                            args=self._getModellerArgs(), cwd=self._getPath())
 
@@ -189,11 +220,19 @@ class ModellerComparativeModelling(EMProtocol):
         return warns
     
     # --------------------------- UTILS functions ------------------------
-    def getTargetID(self):
-        return self.inputSequence.get().getId()
+    def getTargetID(self, seqObj=None):
+        if not seqObj:
+            if not self.multiChain:
+                seqObj = self.inputSequence.get()
+            else:
+                for seqObj in self.inputSequences.get():
+                    pass
+        return seqObj.getId()
 
-    def getTargetSequence(self):
-        return self.inputSequence.get().getSequence()
+    def getTargetSequence(self, seqObj=None):
+        if not seqObj:
+            seqObj = self.inputSequence.get()
+        return seqObj.getSequence()
 
     def _getModellerArgs(self):
         args = ''
@@ -243,38 +282,79 @@ class ModellerComparativeModelling(EMProtocol):
 
     def buildAlignFile(self):
         alignFile = self.getAlignmentFile()
-        seqDic = {}
-        if self.getEnumText('alignMethod') == CUSTOM:
-            seqDic = self.parseInputAlignment()
-        elif self.getEnumText('alignMethod') in [CLUSTALO, MUSCLE, MAFFT]:
-            seqDic = self.makeScipionAlignment()
+        if not self.multiChain:
+            if self.getEnumText('alignMethod') == CUSTOM:
+                seqDic = self.parseInputAlignment()
 
-        with open(alignFile, 'w') as f:
-            targetSeq = self.getTargetSequence()
-            if self.getTargetID() in seqDic:
-                targetSeq = seqDic[self.getTargetID()]
+            elif self.getEnumText('alignMethod') in [CLUSTALO, MUSCLE, MAFFT]:
+                seqDic = self.makeScipionAlignment()
 
-            f.write('>P1;{}\nsequence::::::{}:::\n{}*\n'.
-                    format(self.getTargetID(), self.inputSequence.get().getSeqName(), targetSeq))
+            with open(alignFile, 'w') as f:
+                targetSeq = self.getTargetSequence()
+                if self.getTargetID() in seqDic:
+                    targetSeq = seqDic[self.getTargetID()]
 
-            for tempLine in self.templateList.get().split('\n'):
-              if tempLine.strip():
-                  tempJson = json.loads(tempLine.split(')')[1].strip())
-                  pdbCode, chain = tempJson['pdbName'], tempJson['chain']
-                  idxs = tempJson['index'].split('-')
+                f.write('>P1;{}\nsequence::::::{}:::\n{}*\n'.
+                        format(self.getTargetID(), self.inputSequence.get().getSeqName(), targetSeq))
 
-                  tempSeq = ''
-                  if pdbCode in seqDic:
+                for tempLine in self.templateList.get().split('\n'):
+                  if tempLine.strip():
+                      tempJson = json.loads(tempLine.split(')')[1].strip())
+                      pdbCode, chain = tempJson['pdbName'], tempJson['chain']
+                      idxs = tempJson['index'].split('-')
+
                       tempSeq = seqDic[pdbCode]
+                      f.write('>P1;{}\nstructureX:{}:{}:{}:{}:{}:{}:::\n{}*\n'.
+                              format(pdbCode, pdbCode, idxs[0], chain, idxs[1], chain, pdbCode, tempSeq))
 
+        else:
+          if self.getEnumText('alignMethod') == CUSTOM:
+              shutil.copy(self.inputAlignFile.get(), alignFile)
+              return alignFile
+          elif self.getEnumText('alignMethod') in [CLUSTALO, MUSCLE, MAFFT]:
+              seqDic = self.makeScipionAlignment()
 
-                  f.write('>P1;{}\nstructureX:{}:{}:{}:{}:{}:{}:::\n{}*\n'.
-                          format(pdbCode, pdbCode, idxs[0], chain, idxs[1], chain, pdbCode, tempSeq))
+          with open(alignFile, 'w') as f:
+              seqStr = ''
+              for targetSeqObj in self.inputSequences.get():
+                targetSeq = self.getTargetSequence(targetSeqObj).strip()
+                seqStr += '{}/'.format(targetSeq)
+
+              f.write('>P1;{}\nsequence::::::{}:::\n{}*\n'.
+                      format(self.getTargetID(targetSeqObj), self.getTargetID(targetSeqObj), seqStr[:-1]))
+
+              for tempLine in self.templateList.get().split('\n'):
+                if tempLine.strip():
+                  tempJson = json.loads(tempLine.split(')')[1].strip())
+                  pdbCode = tempJson['pdbName']
+
+                  nChains = len(tempJson['chains'].split(','))
+                  first = True
+                  chains = []
+                  chainsSeqs = ''
+                  for j in range(nChains):
+                      chainId = tempJson['chains'].split(',')[j].strip().split('-')[1]
+                      protSeqCode = '{}_{}'.format(pdbCode, chainId)
+                      if first:
+                          chains.append(chainId)
+
+                      if protSeqCode in seqDic:
+                          tempSeq = seqDic[protSeqCode]
+                      else:
+                          tempSeq = ''  # todo: automatic alignment
+                      chainsSeqs += '{}/'.format(tempSeq)
+
+                      first = False
+
+                  chains.append(chainId)
+
+                  f.write('>P1;{}\nstructureX:{}::{}::{}:{}:::\n{}*\n'.
+                          format(pdbCode, pdbCode, chains[0], chains[1], pdbCode, chainsSeqs[:-1]))
 
         return alignFile
 
-    def getScipionAlignFile(self):
-        return self._getExtraPath('alignment.aln')
+    def getScipionAlignFile(self, idx=''):
+        return self._getExtraPath('alignment{}.aln'.format(idx))
 
     def parseInputAlignment(self):
         seqDic = {}
@@ -283,35 +363,59 @@ class ModellerComparativeModelling(EMProtocol):
         return seqDic
 
     def makeScipionAlignment(self):
-        inpSeqsFile = self._getTmpPath('inputSeqs.fa')
-        with open(inpSeqsFile, 'w') as f:
-            f.write('>{}\n{}\n'.
-                    format(self.getTargetID(), self.getTargetSequence()))
+        if not self.multiChain:
+            inpSeqsFile = self._getTmpPath('inputSeqs.fa')
+            with open(inpSeqsFile, 'w') as f:
+                f.write('>{}\n{}\n'.
+                        format(self.getTargetID(), self.getTargetSequence()))
 
-            for tempLine in self.templateList.get().split('\n'):
-              if tempLine.strip():
-                  tempJson = json.loads(tempLine.split(')')[1].strip())
-                  seqFile = tempJson['seqFile']
-                  with open(seqFile) as fIn:
-                    f.write(fIn.read().strip() + '\n')
+                for tempLine in self.templateList.get().split('\n'):
+                  if tempLine.strip():
+                      tempJson = json.loads(tempLine.split(')')[1].strip())
+                      seqFile = tempJson['seqFile']
+                      with open(seqFile) as fIn:
+                        f.write(fIn.read().strip() + '\n')
 
-        alignFile = self.getScipionAlignFile()
+            seqDic = self.performAlignment(inpSeqsFile)
+            return seqDic
+
+        else:
+            seqDic = {}
+            for i, inSeq in enumerate(self.inputSequences.get()):
+                inpSeqsFile = self._getTmpPath('inputSeqs_{}.fa'.format(i))
+                with open(inpSeqsFile, 'w') as f:
+                    f.write('>{}\n{}\n'.format(self.getTargetID(inSeq), self.getTargetSequence(inSeq)))
+
+                    for tempLine in self.templateList.get().split('\n'):
+                        if tempLine.strip():
+                          tempJson = json.loads(tempLine.split(')')[1].strip())
+                          seqFileTemplate = tempJson['seqFiles']
+                          chainId = tempJson['chains'].split(',')[i].strip().split('-')[1]
+                          seqFileTemplate = seqFileTemplate.replace('_*_*', '_{}_*'.format(chainId))
+                          seqFile = glob.glob(seqFileTemplate)[0]
+                          with open(seqFile) as fIn:
+                            f.write(fIn.read().strip() + '\n')
+
+                iSeqDic = self.performAlignment(inpSeqsFile, idx=str(i))
+                seqDic.update(iSeqDic)
+
+            return seqDic
+
+
+    def performAlignment(self, inpFile, idx=''):
+        alignFile = self.getScipionAlignFile(idx)
         programName = self.getEnumText('alignMethod')
         if programName == CLUSTALO:
-            cline = 'clustalo -i {} --auto -o {} --outfmt=clu'.format(inpSeqsFile, alignFile)
+          cline = 'clustalo -i {} --auto -o {} --outfmt=clu'.format(inpFile, alignFile)
         elif programName == MUSCLE:
-            alignFile = alignFile.replace('.aln', '.fa')
-            cline = 'muscle -align {} -output {}'.format(inpSeqsFile, alignFile)
+          alignFile = alignFile.replace('.aln', '.fa')
+          cline = 'muscle -align {} -output {}'.format(inpFile, alignFile)
         elif programName == MAFFT:
-            cline = 'mafft --auto --clustalout {} > {}'.format(inpSeqsFile, alignFile)
+          cline = 'mafft --auto --clustalout {} > {}'.format(inpFile, alignFile)
         self.runJob(cline, '')
 
         if programName == MUSCLE:
-            seqDic = parseFasta(alignFile)
+          seqDic = parseFasta(alignFile)
         else:
-            seqDic = parseAlnFile(alignFile)
-
+          seqDic = parseAlnFile(alignFile)
         return seqDic
-
-
-
